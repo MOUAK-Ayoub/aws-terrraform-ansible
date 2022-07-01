dd5c5eb2ca7b6f06f64521e9353fa48c54968aa4
ayoub <ayoub.mouak.2015@gmail.com>
2022-07-01 22:23:10 +0000

    add .gitignore'

--- providers.tf
+++ providers.tf
@@ -1,16 +1,15 @@
 provider "aws" {
 
-      region=var.region-master
-      profile=var.profile
-      alias="region-master"
+  region  = var.region-master
+  profile = var.profile
+  alias   = "region-master"
 }
 
 provider "aws" {
 
-      region=var.region-worker
-      profile=var.profile
-      alias="region-worker"
-
+  region  = var.region-worker
+  profile = var.profile
+  alias   = "region-worker"
 }
 
 
