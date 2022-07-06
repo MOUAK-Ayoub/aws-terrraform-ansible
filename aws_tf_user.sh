3b8df9bcb75aca878300f1dc0c18f5a21a3ea4f4
Utilisateur <ayoub.mouak.2015@gmail.com>
2022-07-06 13:33:15 +0100

    aws user and policy creation as a shell script

--- aws_tf_user.sh
+++ aws_tf_user.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 aws iam create-policy --policy-name TfUserPolicy \
  --policy-document file:///home/ansible/aws-terrraform-ansible/TfUserPolicy.json
@@ -7,9 +7,6 @@ terraform_policy_arn=$(aws iam list-policies --query 'Policies[?PolicyName==`"Tf
 aws iam create-user --user-name terraform
 
 aws iam attach-user-policy --policy-arn $(terraform_policy_arn) --user-name terraform
-
-
-
 
 
 aws s3api create-bucket --bucket terraformstatefile2022
